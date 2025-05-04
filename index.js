4fea34e0e71541fbf7b4c25cb433983796f413b4
Manoj Pawar <mp2838946@gmail.com>
2025-05-04 13:24:23 +0530

    updated h1 tag in js

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
 } else if (randomNumber1 < randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
 } else {
     document.querySelector("h1").innerHTML = "Draw!";
 }
